9cdb230d81af369fa61918892b1eab835198d4a5
Johnny Piette <johnny.piette@gmail.com>
2022-05-06 13:21:28 +0200

    Updates

--- Cours/2022/Git/Install/win.cmd
+++ Cours/2022/Git/Install/win.cmd
@@ -1,17 +1,11 @@
 REM gh repo create EqlaExercices --public --clone
 REM @echo off
 cls
+cd %userprofile%\Documents
 mkdir EqlaExercices
 IF EXIST EqlaExercices (
 cd EqlaExercices
-mkdir js
-mkdir java
-mkdir git
-mkdir github
-mkdir html
-mkdir css
-mkdir db
-mkdir laravel
+mkdir js java git github html css db laravel
 
 curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/.gitignore.txt -o .gitignore
 curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
