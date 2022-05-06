2ea4c263ede8776b5f2815daf2cb2d462b89e81a
Johnny Piette <johnny.piette@gmail.com>
2022-05-06 14:55:10 +0200

    Updates

--- Cours/2022/Git/Install/win.cmd
+++ Cours/2022/Git/Install/win.cmd
@@ -3,16 +3,16 @@ IF NOT EXISTS %userprofile%\Documents\EqlaExercices (
     gh repo create EqlaExercices --public --clone
     IF EXIST EqlaExercices (
         cd EqlaExercices
-        mkdir js java git github html accessibilite css db laravel Intro 2>NUL
+        mkdir js java git github html accessibilite css db laravel intro
 
         curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/gitignore.txt -o .gitignore
         curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
         
         update.cmd
-
-        del /q ..\win.cmd
         IF NOT EXIST update.cmd (
             echo problème pour télécharger le fichier update.cmd
         )
+
+        del /q ..\win.cmd
     )
 )
