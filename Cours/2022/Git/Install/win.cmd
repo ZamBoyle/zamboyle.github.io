df04b25e4418ca782f32518ac77fa522ae018c0d
Johnny Piette <johnny.piette@gmail.com>
2022-05-06 13:26:09 +0200

    Updates

--- Cours/2022/Git/Install/win.cmd
+++ Cours/2022/Git/Install/win.cmd
@@ -5,7 +5,7 @@ cd %userprofile%\Documents
 mkdir EqlaExercices
 IF EXIST EqlaExercices (
 cd EqlaExercices
-mkdir js java git github html css db laravel 2>NUL
+mkdir js java git github html accessibilite css db laravel Intro 2>NUL
 
 curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/gitignore.txt -o .gitignore
 curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
