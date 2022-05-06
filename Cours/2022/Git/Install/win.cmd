884d570815c076ed160a4cafa4282a8205414e81
Johnny Piette <johnny.piette@gmail.com>
2022-05-06 13:49:33 +0200

    Updates

--- Cours/2022/Git/Install/win.cmd
+++ Cours/2022/Git/Install/win.cmd
@@ -1,14 +1,14 @@
 cd %userprofile%\Documents
 gh repo create EqlaExercices --public --clone
 IF EXIST EqlaExercices (
-cd EqlaExercices
-mkdir js java git github html accessibilite css db laravel Intro 2>NUL
+    cd EqlaExercices
+    mkdir js java git github html accessibilite css db laravel Intro 2>NUL
 
-curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/gitignore.txt -o .gitignore
-curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
+    curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/gitignore.txt -o .gitignore
+    curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
 
-del /q ..\win.cmd
-IF NOT EXIST update.cmd (
-   echo problème pour télécharger le fichier update.cmd
-)
+    del /q ..\win.cmd
+    IF NOT EXIST update.cmd (
+        echo problème pour télécharger le fichier update.cmd
+    )
 )
