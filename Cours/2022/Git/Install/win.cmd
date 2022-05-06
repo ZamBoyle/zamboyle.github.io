445df4b859000e3a6c1d00d107f44b373c43df70
Johnny Piette <johnny.piette@gmail.com>
2022-05-06 14:13:37 +0200

    Updates

--- Cours/2022/Git/Install/win.cmd
+++ Cours/2022/Git/Install/win.cmd
@@ -7,6 +7,8 @@ IF NOT EXISTS %userprofile%\Documents\EqlaExercices (
 
         curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/gitignore.txt -o .gitignore
         curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
+        
+        update.cmd
 
         del /q ..\win.cmd
         IF NOT EXIST update.cmd (
