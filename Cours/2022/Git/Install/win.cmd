9ac7ca2bb3409bea4199b536b23bd3194232ec24
Johnny Piette <johnny.piette@gmail.com>
2022-05-06 13:49:06 +0200

    Updates

--- Cours/2022/Git/Install/win.cmd
+++ Cours/2022/Git/Install/win.cmd
@@ -7,7 +7,7 @@ mkdir js java git github html accessibilite css db laravel Intro 2>NUL
 curl -f -s https://zamboyle.github.io/Cours/2022/Git/Install/gitignore.txt -o .gitignore
 curl -O -f -s https://zamboyle.github.io/Cours/2022/Git/Install/update.cmd
 
-rd /s /q ..\win.cmd
+del /q ..\win.cmd
 IF NOT EXIST update.cmd (
    echo problème pour télécharger le fichier update.cmd
 )
