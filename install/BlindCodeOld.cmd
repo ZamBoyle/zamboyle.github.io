00411f9424fde410c466ce0d21d6d8b48587126f
Johnny Piette <johnny.piette@gmail.com>
2022-05-12 13:52:31 +0200

    Updates

--- install/BlindCodeOld.cmd
+++ install/BlindCodeOld.cmd
@@ -1,6 +1,7 @@
 @Echo off
 SetLocal EnableDelayedExpansion
 
+
 :Main
 CALL :IsVSCodeInstalled
 
@@ -44,6 +45,20 @@ gh --version >nul 2>&1 && (
     EXIT /B 1
 )
 
+:IsVSCodeInstalled
+set VSCODE_DEV=
+set ELECTRON_RUN_AS_NODE=1
+REM "%userprofile%\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd" --version >nul 2>&1 && (
+REM "%userprofile%\AppData\Local\Programs\Microsoft VS Code\Code.exe" "%userprofile%\AppData\Local\Programs\Microsoft VS Code\resources\app\out\cli.js" --ms-enable-electron-run-as-node --version >nul 2>&1 && (
+IF EXIST "%userprofile%\AppData\Local\Programs\Microsoft VS Code\Code.exe" (
+    EXIT /B 0
+) ELSE (
+    EXIT /B 1
+)
+
+
+
+
 :git
 echo Telechargement de Git
 curl -L https://github.com/git-for-windows/git/releases/download/v2.36.0.windows.1/Git-2.36.0-64-bit.exe -o gitinstall.exe
@@ -65,6 +80,31 @@ echo Installation de Gh terminee
 echo.
 EXIT /B O
 
+:VSCode
+echo Telechargement de VSCode
+curl -L "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" -o vscode.exe
+echo Installation de VSCode. Veuillez patienter.
+vscode /VERYSILENT /NORESTART /MERGETASKS=!runcode
+echo Installation de VSCode terminee
+echo.
+EXIT /B O
+
+:NVDA
+echo Telechargement de NVDA
+curl -L https://www.nvda-fr.org/r?p=nvda -o nvda.exe
+echo Installation de NVDA. Veuillez patienter.
+nvda --minimal --install-silent
+echo Installation de NVDA terminee
+echo.
+EXIT /B O
+
+:ExtensionsVSCode
+echo Installation des extensions VSCode
+for %%i in (ms-ceintl.vscode-language-pack-fr vscjava.vscode-java-pack onecentlin.laravel-extension-pack) do call "%userprofile%\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd" --force --install-extension %%i
+echo Fin d'installation des extensions VSCode
+echo.
+EXIT /B O
+
 :ConfigureGit
 git config --global --unset-all user.name
 git config --global --unset-all user.email
@@ -78,6 +118,17 @@ EXIT /B O
 gh auth login -s delete_repo
 EXIT /B O
 
+:EECreateRepositoryOLD
+IF NOT EXIST %USERPROFILE%\Documents\EqlaExercices(
+    Mkdir %USERPROFILE%\Documents\EqlaExercices
+    cd %USERPROFILE%\Documents\EqlaExercices
+) ELSE (
+    IF NOT EXIST .git (
+
+    )
+)
+EXIT /B 0
+
 :CreateRepo
     if NOT EXIST %userprofile%\Documents\EqlaExercices (
         cd %userprofile%\Documents\
@@ -87,6 +138,7 @@ EXIT /B O
 
         if EXIST EqlaExercices (
             cd EqlaExercices
+            rem mkdir js java git github html accessibilite css db laravel intro
             for %%i in (js java git github html accessibilite css db laravel intro) do mkdir %%i && type NUL > %%i\.gitkeep && attrib +h %%i\.gitkeep
 
             echo @echo off >> EqlaUpdate.cmd
@@ -109,6 +161,26 @@ EXIT /B O
     )
 EXIT /B 0
 
+:OpenJDK
+REM curl -L https://aka.ms/download-jdk/microsoft-jdk-17.0.2.8.1-windows-x64.msi -o MSOpenJDK.msi
+echo Telechargement d'OpenJDK
+curl -L https://download.java.net/java/GA/jdk18.0.1.1/65ae32619e2f40f3a9af3af1851d6e19/2/GPL/openjdk-18.0.1.1_windows-x64_bin.zip -o openjdk.zip
+rem culr -L https://aka.ms/download-jdk/microsoft-jdk-11.0.14.1_1-31205-windows-x64.msi -o ms-openjdk.msi
+echo Fin du telechargement d'OpenJDK
+echo Decompression d'openjdk
+tar -zxvf openjdk.zip
+echo Decompression terminee
+move jdk-18.0.1.1 C:\PROGRA~1
+REM setx /m PATH "%PATH%;C:\Progra~1\jdk-18.0.1.1\bin"
+setx /m JAVA_HOME "C:\Progra~1\jdk-18.0.1.1"
+set pathkey="HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment"
+for /F "usebackq skip=2 tokens=2*" %%A IN (`reg query %pathkey% /v Path`) do (reg add %pathkey% /f /v Path /t REG_SZ /d "%%B;C:\Progra~1\jdk-18.0.1.1\bin")
+rem msiexec ms-openjdk.msi /i ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome INSTALLDIR="c:\Program Files\Microsoft\" /quiet
+
+REM or set pathkey="HKEY_CURRENT_USER\Environment" for user path.
+REM powershell -command "& {$md=\"[DllImport(`\"user32.dll\"\",SetLastError=true,CharSet=CharSet.Auto)]public static extern IntPtr SendMessageTimeout(IntPtr hWnd,uint Msg,UIntPtr wParam,string lParam,uint fuFlags,uint uTimeout,out UIntPtr lpdwResult);\"; $sm=Add-Type -MemberDefinition $md -Name NativeMethods -Namespace Win32 -PassThru;$result=[uintptr]::zero;$sm::SendMessageTimeout(0xffff,0x001A,[uintptr]::Zero,\"Environment\",2,5000,[ref]$result)}"
+EXIT /B 0
+
 :Menu
 echo =========================
 echo = BlindCode - Mons 2022 =
@@ -116,6 +188,11 @@ echo =========================
 
 CALL :IsGitInstalled && ( echo 1. Installer Git : DEJA INSTALLE.) || ( echo 1. Installer Git)
 CALL :IsGhInstalled && ( echo 2. Installer Gh : DEJA INSTALLE.) || ( echo 2. Installer Gh)
+REM CALL :IsVSCodeInstalled && ( echo 3. Installer VSCode : DEJA INSTALLE) || ( echo 3. Installer VSCode)
+REM echo 4. Installer VSCode Extensions: Java, Langue FR, Laravel Extension Pack
+REM echo 5. Installer OpenJDK d'Oracle
+REM echo 6. Installer NVDA
+REM echo =========================================
 CALL :IsGitInstalled && (
     CALL :IsGitConfigured && ( 
         echo 3. Configurer Git: DEJA CONFIGURE.
@@ -131,11 +208,14 @@ CALL :IsGhInstalled && (
         echo 4. Configurer Gh
     )
 )
+REM CALL :IsGitConfigured || CALL :IsAuth (echo =========================================)
 
 CALL :IsAuth && CALL :IsGitConfigured && (
     echo 5. Creer le depot EqlaExercice sur GitHub.    
-    ) || (
+) || (
+    REM echo 5. Creer le depot EqlaExercice sur GitHub [Git et Gh doivent configures avant !]
 )
+REM CALL :IsGitConfigured && (CALL :IsAuth ||(echo =========================================))
 echo =========================
 echo 6. Quitter
 echo.
@@ -147,6 +227,11 @@ if %choix% GEQ 7 CALL :Menu
 
 if %choix%  EQU 1 CALL :Git
 if %choix%  EQU 2 CALL :Gh
+REM if %choix%  EQU 3 CALL :VSCode
+REM if %choix%  EQU 4 CALL :ExtensionsVSCode
+REM if %choix%  EQU 5 CALL :OpenJDK
+REM if %choix%  EQU 6 CALL :NVDA
+
 if %choix%  EQU 3 CALL :ConfigureGit
 if %choix%  EQU 4 CALL :ConfigureGh
 if %choix%  EQU 5 CALL :CreateRepo
