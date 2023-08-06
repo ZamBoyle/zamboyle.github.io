bd60de02d37f7a3d7dc8dc363405931bbb01ba4d
Johnny Piette <johnny.piette@gmail.com>
2023-08-06 21:17:02 +0200

    Updates

--- qrTomatoes/src/db.js
+++ qrTomatoes/src/db.js
@@ -4,7 +4,7 @@ export const tomatoes = [
         nom: "Tomate Maya F1",
         description:
             "Savoureuse tomate rayée brun-jaune, très décorative dans le potager! Cette variété a de beaux fruits ronds, jaunes avec des rayures brunes à violettes à partir du calice.",
-        urlImage: "https://www.vivavera.be/foto/fotogroot/3705gr.jpg",
+        urlImage: "../img/1_Maya_F1.jpg",
     },
     {
         id: 2,
@@ -19,11 +19,6 @@ export const tomatoes = [
         description:
             "<p>La tomate ananas est une variété tardive produisant des fruits de 250 à 400 grammes en moyenne. Photo par Jean Weber.</p>"+
             "<p>Avec sa couleur jaune et son bon goût sucré, la tomate ananas est une variété de tomates anciennes dont la chair très dense ressemble à une tranche d'ananas. Originaire des États-Unis, elle est très parfumée et contient peu de graines.</p>"+
-            "<p>On aime la tomate ananas en salade ou en carpaccio, avec juste un filet d’huile d’olive extra vierge, du sel et du poivre.</p>"+
-            "<p>On aime la tomate ananas en salade ou en carpaccio, avec juste un filet d’huile d’olive extra vierge, du sel et du poivre.</p>"+
-            "<p>On aime la tomate ananas en salade ou en carpaccio, avec juste un filet d’huile d’olive extra vierge, du sel et du poivre.</p>"+
-            "<p>On aime la tomate ananas en salade ou en carpaccio, avec juste un filet d’huile d’olive extra vierge, du sel et du poivre.</p>"+
-            "<p>On aime la tomate ananas en salade ou en carpaccio, avec juste un filet d’huile d’olive extra vierge, du sel et du poivre.</p>"+
             "<p>On aime la tomate ananas en salade ou en carpaccio, avec juste un filet d’huile d’olive extra vierge, du sel et du poivre.</p>",
         urlImage: "https://www.ecoumene.com/wp-content/uploads/2021/12/tomate-ananas.jpg",
     },            
@@ -40,6 +35,6 @@ export const tomatoes = [
         nom: "Tomate Jaune",
         description:
             "La tomate jaune est une variété de tomate de couleur jaune. Elle est plus douce que la tomate rouge et a une saveur plus sucrée. Elle est souvent utilisée dans les salades et les sauces.",
-        urlImage: "https://www.vivavera.be/foto/fotogroot/3680gr.jpg",
+        urlImage: "https://www.labrouettemaraichere.com/cdn/shop/products/unnamed_ea0e6b83-7570-4879-97b0-b7646f4ce24b_373x.jpg?v=1586112287",
     },
 ];
