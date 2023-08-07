81139e39c1e42449d1b8f198d79ffc40dedb6ecc
Johnny Piette <johnny.piette@gmail.com>
2023-08-07 07:16:59 +0200

    Updates

--- qrTomatoes/js/app.js
+++ qrTomatoes/js/app.js
@@ -27,49 +27,25 @@ function loadImage(tomato) {
   });
 }
 const body = document.getElementsByTagName("h1")[0];
-//const nl = "<br>";
-//body.innerHTML = "1. Begin" + nl;
-/*
-if (navigator && navigator.mediaDevices){// && navigator.mediaDevices.enumerateDevices) {
-    navigator.mediaDevices
-        .enumerateDevices()
-        .then(function (devices) {
-            body.innerHTML += "2. enumerateDevices" + nl;
-            const hasCamera = devices.some(device => device.kind === "videoinput");
-            if (hasCamera) {
-                body.innerHTML += "3. Caméra détectée!" + nl;
-            } else {
-                body.innerHTML += "3. Aucune caméra trouvée." + nl;
-            }
-        })
-        .catch(function (error) {
-            body.innerHTML += "3. Une erreur s'est produite : " + error + nl;
-        });
-} else {
-    body.innerHTML += "2. Pas de navigator" + nl;
-}
-body.innerHTML += "4. End" + nl;
-*/
-//body.innerHTML = navigator.mediaDevices ? "2. navigator.mediaDevices" + nl : "2. Pas de navigator" + nl;
+
 navigator.mediaDevices
   .enumerateDevices()
   .then(function (devices) {
     let cameraId;
-    //body.innerHTML += devices.length + " caméras trouvées"+nl;
 
-    // Trouvez l'id de la caméra arrière
+    // Trouver l'id de la caméra arrière
     devices.forEach(function (device) {
       if (device.kind === "videoinput" && device.label.includes("back")) {
         cameraId = device.deviceId;
       }
     });
 
-    // Si vous n'avez pas trouvé la caméra arrière, utilisez simplement la première caméra vidéo
+    // Si on  n'a pas trouvé la caméra arrière, on utilise simplement la première caméra vidéo
     if (!cameraId && devices.length > 0) {
       cameraId = devices[0].deviceId;
     }
 
-    // Demandez l'accès à la caméra
+    // Demander l'accès à la caméra
     return navigator.mediaDevices.getUserMedia({
       video: {
         deviceId: cameraId ? { exact: cameraId } : undefined,
@@ -77,13 +53,13 @@ navigator.mediaDevices
     });
   })
   .then(function (stream) {
-    // Utilisez le flux de la caméra comme avant
+    // Utiliser le flux de la caméra comme avant
     const videoElement = document.getElementById("camera");
     videoElement.srcObject = stream;
   })
   .catch(function (error) {
     if (error.name === "OverconstrainedError") {
-      // Tentez d'accéder à la caméra avec des contraintes moins restrictives
+      // Tenter d'accéder à la caméra avec des contraintes moins restrictives
       return navigator.mediaDevices.getUserMedia({ video: true });
     } else {
       console.log("Une erreur s'est produite : " + error);
@@ -99,27 +75,6 @@ const resultElement = document.getElementById("result");
 
 const overlayElement = document.getElementById("overlay");
 const overlay = overlayElement.getContext("2d");
-/* 
-         function tick() {
-              if (video.readyState === video.HAVE_ENOUGH_DATA) {
-                  canvasElement.hidden = false;
-  
-                  canvasElement.height = video.videoHeight;
-                  canvasElement.width = video.videoWidth;
-                  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-  
-                  const imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-                  const code = jsQR(imageData.data, imageData.width, imageData.height, {
-                      inversionAttempts: 'dontInvert',
-                  });
-  
-                  if (code) {
-                      resultElement.innerHTML = getTomatoInfo(code.data);
-                  }
-              }
-  
-              requestAnimationFrame(tick);
-          }  */
 
 function tick() {
   if (video.readyState === video.HAVE_ENOUGH_DATA) {
@@ -176,19 +131,6 @@ function tick() {
         (tomato) => tomato.id == parseInt(code.data)
       );
       if (tomato) {
-        /*                         let h1 = document.querySelector('h1');
-                                        let p = document.createElement('p');
-                                        p.innerHTML = tomato.urlImage;
-                                        h1.appendChild(p); */
-        // On charge l'image de la tomate
-        /*                         let image = new Image();
-                                        image.onload = function () {
-                                            // Une fois l'image chargée, on la dessine au-dessus du rectangle
-                                            let imageX = code.location.topLeftCorner.x;
-                                            let imageY = code.location.topLeftCorner.y - image.height; // On dessine l'image juste au-dessus du rectangle
-                                            overlay.drawImage(image, imageX, imageY, image.width, image.height);
-                                        };
-                                        image.src = tomato.urlImage; // On suppose que imageUrl est une propriété de l'objet tomato     */
         // Création d'une fonction asynchrone à l'intérieur de tick
         (async function () {
           const image = await loadImage(tomato);
