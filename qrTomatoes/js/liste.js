a557d567c1f0661d5599c094e2f63562a64e9c7a
Johnny Piette <johnny.piette@gmail.com>
2023-08-11 15:34:05 +0200

    Updates

--- qrTomatoes/js/liste.js
+++ qrTomatoes/js/liste.js
@@ -1,18 +1,19 @@
 import * as db from './db.js';
-import { planted } from './planted.js';
+import * as planted  from './planted.js';
 
 const tomatoesListDiv = document.getElementById('tomatoesList');
 
-planted.forEach(x => {
+planted.getCurrentPlantedDetails().plants.forEach(x => {
     const tomato = db.getTomatoInfo(x);
     if (!tomato) return;
+    const size = tomato.urlImage === '0_Unknown.jpg' ? 'style="width:50px"' : 'style="width:200px"';
     const tomatoCard = `
         <div class="col">
             <div class="card h-100 shadow">
-                <img src="../img/${tomato.urlImage}" alt="${tomato.nom}" class="card-img-top">
+                <img src="../img/${tomato.urlImage}" alt="${tomato.nom}" class="card-img-top" ${size}">
                 <div class="card-body">
                     <h5 class="card-title">${tomato.nom}</h5>
-                    <p class="card-text">${tomato.description.length > 80 ? tomato.description.substring(0, 77) + '...' : tomato.description}</p>
+                    <p class="card-text">${tomato.description}</p>
                 </div>
             </div>
         </div>
