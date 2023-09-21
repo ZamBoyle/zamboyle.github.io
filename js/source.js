5ed2f08fe28c2aa210173b05f4d6e15b59619904
Johnny Piette <johnny.piette@gmail.com>
2023-09-21 23:43:52 +0200

    Updates

--- js/source.js
+++ js/source.js
@@ -3,14 +3,14 @@ window.onload = function() {
     var container = document.createElement('div');
     container.id = 'source-code-container';
     container.style = `border: 4px solid ;padding: 10px;font-weight: bold; font-size: 20px;`;
-    document.body.appendChild(container);
+//    document.body.appendChild(container);
 
     // Obtenez le code source de la page
     var html = document.documentElement.outerHTML;
     
     // Retirez toutes les balises <script src="..."></script> du code source
     var cleanedHtml = html.replace(/<script[\s\S]*?<\/script>/gi, '');
-    html = html.replace(/<div id="source-code-container">[\s\S]*?<\/div>/, '');
+    //html = html.replace(/<div id="source-code-container">[\s\S]*?<\/div>/, '');
     
     // Échappez les caractères spéciaux pour qu'ils soient affichés correctement
     var escapedHtml = cleanedHtml
@@ -22,4 +22,5 @@ window.onload = function() {
     
     // Insérez le code source échappé dans le div
     container.innerHTML = `<pre style="word-break: break-all;white-space: pre-wrap;">${escapedHtml}</pre>`;
+    document.body.appendChild(container);
   }
