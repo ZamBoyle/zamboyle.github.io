63cf8933e7a9582af837fbb3bd5437542e4c358d
Johnny Piette <johnny.piette@gmail.com>
2023-09-22 00:13:13 +0200

    Updates

--- js/source.js
+++ js/source.js
@@ -7,18 +7,16 @@ window.onload = function() {
     var container = document.createElement('div');
     container.id = 'source-code-container';
     container.style = `border: 4px solid ;padding: 10px;font-weight: bold; font-size: 20px;`;
-//    document.body.appendChild(container);
 
     // Obtenez le code source de la page
     var html = "<!DOCTYPE html>\n" + document.documentElement.outerHTML;
-
-    //html = html.replace(/>/g, '>\n');
 
     // Remove the base tag from the html string
     html = html.replace(/<base[^>]*>/gi, '');
 
     
     // Retirez toutes les balises <script src="..."></script> du code source
+    // Dans le but de ne pas perturber les étudiants.
     var cleanedHtml = html.replace(/<script[\s\S]*?<\/script>/gi, '');
     //html = html.replace(/<div id="source-code-container">[\s\S]*?<\/div>/, '');
     
