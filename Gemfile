0312a0b906c6d875419241b48483f7e95f12e70b
Johnny Piette <johnny.piette@gmail.com>
2023-11-22 22:00:43 +0100

    Updates

---
+++ Gemfile
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+source "https://rubygems.org"
+
+gem "jekyll", "~> 3.9.3"
+gem "jekyll-email-protect"
+gem "jekyll-github-metadata"
+gem "jekyll-paginate-v2"
+gem "jekyll-scholar"
+gem "jekyll-twitter-plugin"
+gem "jemoji"
+# Il semble que 'bootstrap' soit utilisé comme thème ou plugin. Si c'est un plugin Ruby, décommentez la ligne suivante. Sinon, vous devrez l'intégrer via les fichiers de thème.
+# gem "bootstrap"
+
+# Si vous utilisez 'material-sass' comme thème ou plugin, assurez-vous qu'il s'agit d'une gem valide et décommentez la ligne suivante.
+# gem "material-sass"
+
+# Ajouter des dépendances pour les fonctionnalités optionnelles si nécessaire
+gem "mathjax-rails" 
+
+# Assurez-vous que ces versions de librairies sont disponibles sous forme de gems ou intégrez-les directement dans vos fichiers de thème.
+# gem "academicons", "~> 1.9.0"
+# gem "fontawesome", "~> 5.14.0"
+# gem "jquery-rails", "~> 3.5.1"
+# gem "mathjax-rails", "~> 3.1.2"
+# gem "mdb", "~> 4.19.1"
+# gem "popper_js", "~> 2.4.4"
+
+group :jekyll_plugins do
+   gem "jekyll-feed", "~> 0.12"
+end
---
+++ js/menu.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var menuHtml = `
+        <nav class="navbar navbar-expand-lg bg-dark fixed-top" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">BlindCode</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">Menu
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-underline me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link ${isActive('/index.html') ? 'active' : ''}" aria-current="page" href="/index.html">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ${isActive('/pages/horaire.html') ? 'active' : ''}" href="/pages/horaire.html">Horaire des cours</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link ${isActive('/pages/zoom.html') ? 'active' : ''}" href="/pages/zoom.html">Zoom</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link rounded ${isActive('/pages/depots') ? 'active' : ''}" target="_blank" noreferrer
+                                href="/pages/depots.html">Liste des dépôts Github</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link rounded " target="_blank" noreferrer
+                         href="https://kraktu.github.io/Blindcode/output/html/tutoriels/accueil-tutoriels.html">Cours de Philip</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    `;
+    footerHtml = `<footer class="footer mt-auto py-3 bg-dark fixed-bottom" data-bs-theme="dark">
+    <div class="container">
+      <span class="text-white">&copy; 2023&nbsp;<img src="/assets/img/Logo_Eqla.png" height="25">&nbsp;Tous droits réservés.</span>
+    </div>
+  </footer>`;
+
+    var header = document.querySelector('header'); // Ou un autre élément dans lequel vous souhaitez insérer le menu
+    var footer = document.querySelector('main'); // Ou un autre élément dans lequel vous souhaitez insérer le menu
+    if (header) {
+        header.insertAdjacentHTML('beforeend', menuHtml);
+    }
+    if(footer){
+        footer.insertAdjacentHTML('afterend', footerHtml);
+    }
+});
+
+function isActive(path) {
+    return window.location.pathname === path;
+}
